5db6df2f69d676b3d6f3276a13094aac8ad5a5ce
Daniel Wegener <avithan@gmail.com>
2013-03-27 14:08:16 +0100

    Added sample arquillian configuration

---
+++ ranked-impl/src/main/java/de/holisticon/ranked/PlayerResourceImpl.java
@@ -0,0 +1,16 @@
+package de.holisticon.ranked;
+
+import de.holisticon.ranked.api.PlayerResource;
+import de.holisticon.ranked.api.model.Player;
+
+/**
+ * @author Daniel
+ */
+public class PlayerResourceImpl implements PlayerResource {
+
+    @Override
+    public void create(Player payload) {
+        throw new AssertionError("Not yet implemented.");
+    }
+
+}
---
+++ ranked-impl/src/test/java/de/holisticon/ranked/PlayerResourceImplTest.java
@@ -0,0 +1,35 @@
+package de.holisticon.ranked;
+
+import de.holisticon.ranked.api.model.Player;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+/**
+ * @author Daniel
+ */
+@RunWith(Arquillian.class)
+public class PlayerResourceImplTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(PlayerResourceImpl.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    PlayerResourceImpl resource;
+
+
+    @Test
+    public void test() {
+        resource.create(new Player("Name"));
+    }
+}
