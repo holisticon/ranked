81f9f797459d6a277cd1338c29729ca7baf35aba
Daniel Wegener <avithan@gmail.com>
2013-04-07 15:00:40 +0200

    Fixed cargo and arquillian setup

--- ranked-impl/src/test/java/de/holisticon/ranked/PlayerResourceIT.java
+++ ranked-impl/src/test/java/de/holisticon/ranked/PlayerResourceIT.java
@@ -1,18 +1,19 @@
 package de.holisticon.ranked;
 
+import de.holisticon.ranked.api.model.PersistentEntity;
 import de.holisticon.ranked.api.model.Player;
+import de.holisticon.ranked.api.model.Tournament;
 import de.holisticon.ranked.model.PlayerDao;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import scala.Product;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
 
 /**
  * @author Daniel
@@ -23,8 +24,9 @@ public class PlayerResourceIT {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addClass(PlayerDao.class)
-                .addAsManifestResource("META-INF/persistence.xml")
+                .addClasses(PlayerDao.class, PersistentEntity.class, Player.class, Tournament.class)
+                .addAsManifestResource("test-persistence.xml", "persistence.xml")
+                .addAsResource("jbossas-ds.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
