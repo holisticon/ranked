4e53f86db3349f85c24cb8dec4eaf4699ce80cf1
Daniel Wegener <avithan@gmail.com>
2013-04-14 16:35:50 +0200

    added js client prototype and changed from resteasy-servlet to resteasy-filter

---
+++ ranked-webapp/src/main/webapp/js/ranked.js
@@ -0,0 +1,23 @@
+var serverBasePath = 'http://localhost:8080/ranked'
+
+function PlayerController($scope,$http) {
+    $http.get(serverBasePath+'/player').success(function(data) {
+        $scope.players = data;
+    });
+}
+
+function TournamentController($scope) {
+    $.getJSON(serverBasePath+'/tournament', function(data) {
+        $scope.tournaments = data;
+    });
+}
+
+function DisciplineController($scope) {
+    $.getJSON(serverBasePath+'/discipline', function(data) {
+        $scope.disciplines = data;
+    });
+}
+
+
+
+
