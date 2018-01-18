ee23156e785bbb7e4f86125bd0218d6515b4f33f
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-01-18 10:47:05 +0100

    Init react-redux with typescript starter + webpack build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,34 @@
-var path = require('path');
-
 module.exports = {
-  entry: './src/main/js/app.js',
-  devtool: 'sourcemaps',
-  cache: true,
-  debug: true,
+  entry: "./src/index.tsx",
   output: {
-    path: __dirname,
-    filename: './target/webpack/built/bundle.js'
+      filename: "bundle.js",
+      path: __dirname + "/dist"
   },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
   module: {
-    loaders: [
-      {
-        test: path.join(__dirname, '.'),
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
+  },
 };
