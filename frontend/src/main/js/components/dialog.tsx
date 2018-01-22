201616eb3a2cb5cd862dc8e42da719425e2dc580
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-01-22 08:39:24 +0100

    First implementation of dialog + styling

---
+++ frontend/src/main/js/components/dialog.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+export type ButtonType = 'ok' | 'error' | 'warn' | 'grey';
+
+export interface DialogProps {
+  headline: string;
+  text: string;
+  buttons: Array<{
+    text: string,
+    type: ButtonType,
+    click: () => void
+  }>;
+}
+
+export class Dialog extends React.Component<DialogProps> {
+  constructor(props: DialogProps) {
+    super(props);
+  }
+
+  getButtons() {
+    return this.props.buttons.map((btn, index) => {
+      return (
+        <div className={ 'dialog-button button-' + btn.type } key={ index } onClick={ btn.click }>
+          { btn.text }
+        </div>
+      );
+    }); 
+  }
+
+  render() {
+    return (
+      <div className="dialog-container">
+        <div className="dialog-content">
+          <div className="dialog-headline">{ this.props.headline }</div>
+          <div className="dialog-text">{ this.props.text }</div>
+          <div className="dialog-button-container">
+            { this.getButtons() }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
