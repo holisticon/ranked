c74568b32ca612d4517550916807721dcdeefed0
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-01-26 14:47:07 +0100

    Zuul support

--- frontend/src/main/kotlin/FrontendApplication.kt
+++ frontend/src/main/kotlin/FrontendApplication.kt
@@ -2,6 +2,7 @@ package de.holisticon.ranked.frontend
 
 import de.holisticon.ranked.extension.runApplicationExpr
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 
 /**
@@ -10,4 +11,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 fun main(args: Array<String>) = runApplicationExpr<FrontendApplication>(*args)
 
 @SpringBootApplication
+@EnableZuulProxy
 class FrontendApplication
