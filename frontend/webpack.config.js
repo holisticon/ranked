cc685f9f1c67e9847d7b13c23f9d22d141525626
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-02-23 13:52:11 +0100

    Extended our webpack config to allow hot reload for development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,70 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: "./src/main/js/app.tsx",
-  output: {
-      filename: "bundle.js",
-      path: __dirname + "/target/webpack/built"
-  },
+    entry: [
+        "react-hot-loader/patch",
+        "./src/main/js/app.tsx",
+    ],
+    output: {
+        path: path.join(__dirname, 'target/webpack/built'),
+        filename: "bundle.js",
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  devServer: {
-    historyApiFallback: true
-  },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".json"]
+    },
 
-  resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/main/resources/static/index.html')
+        }),
+    ],
 
-  module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            {
+                test: /\.tsx?$/,
+                loaders: [
+                    "react-hot-loader/webpack",
+                    "awesome-typescript-loader"
+                ],
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, "src"),
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+        ]
+    },
 
-          { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
-      ]
-  },
+    devServer: {
+        hot: true,
+        port: 3000,
+        proxy: {
+            "/": "http://localhost:8080"
+        }
+    },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
-  },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
