c8a9e9356f931f32f92775bb6f9372d6d0e6d3a5
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-03-09 21:36:00 +0100

    Added styling for scoreboard

--- frontend/src/main/js/pages/score-board.tsx
+++ frontend/src/main/js/pages/score-board.tsx
@@ -1,46 +1,117 @@
 import * as React from 'react';
 import axios from 'axios';
 import './score-board.css';
+import { Player } from '../types/types';
+import { PlayerIcon } from '../components/player-icon';
 
-type PlayerWithElo = {
-    userName: {
-        value: string;
-    },
-    elo: number;
+type EloData = {
+  userName: {
+    value: string;
+  },
+  elo: number;
 };
 
-export class ScoreBoard extends React.Component<any, { players: Array<PlayerWithElo> }> {
+interface PlayerWithElo extends Player {
+  score: number;
+}
+
+type ScoreBoardState = {
+  players: Array<PlayerWithElo>,
+  maxElo: number,
+  minElo: number
+};
+
+export class ScoreBoard extends React.Component<any, ScoreBoardState> {
   constructor(props: any) {
     super(props);
 
-    this.getPlayersScore();
+    // init data
+    this.getAllPlayers().then(playersMap => {
+      this.getPlayersScore().then(eloData => {
+        const players = eloData.map(elo => {
+          return {
+            ...playersMap[elo.userName.value],
+            score: elo.elo
+          };
+        });
+
+        this.setState({
+          players,
+          maxElo: eloData[0].elo,
+          minElo: eloData[eloData.length - 1].elo
+        });
+      });
+    });
   }
 
-  getPlayersScore(): void {
-    axios.get('/view/elo/player').then(res => this.setState({ players: res.data }));
+  private getAllPlayers(): Promise<{ [id: string]: Player }> {
+    return axios.get('/view/user')
+      .then(res => res.data as Array<Player>)
+      .then(players => players.reduce(
+        (map, player) => {
+          map[player.id] = player;
+          return map;
+        },
+        {})
+      );
   }
 
-  getRanking() {
-      if (!this.state || !this.state.players) {
-          return;
-      }
+  private getPlayersScore(): Promise<Array<EloData>> {
+    return axios.get('/view/elo/player')
+      .then(res => res.data);
+  }
 
-      return this.state.players
-        .filter(player => player.elo !== 1000)
-        .map((player: PlayerWithElo, i: number) => {
-          return (
-              <div key="i">
-                  <span>{ player.elo + ' - ' +  player.userName.value }</span>
-              </div>
-          );
+  private calcEloPercentage(elo: number): number {
+    const min = this.state.minElo;
+    const max = this.state.maxElo;
+
+    return (elo - min) / (max - min);
+  }
+
+  private getRanking() {
+    if (!this.state || !this.state.players) {
+      return;
+    }
+
+    return this.state.players
+      .map((player, i) => {
+        const barWidth = this.calcEloPercentage(player.score) * 70 + 30;
+
+        return (
+          <div key="i" className="ranking-entry">
+            <div className="icon">
+              <PlayerIcon img={player.imageUrl} click={() => { return; }} />
+            </div>
+            <div className="name">{player.name}</div>
+            <div className="bar">
+              <div className="bar-inner" style={{ width: barWidth + '%' }} />
+            </div>
+            <div className="score">{player.score}</div>
+          </div>
+        );
       });
   }
 
-  render() {
+  private getHeaderIconPath(): string {
+    return '/img/trophy.png';
+  }
+
+  private getTitle(): string {
+    return 'Holisticon AllStars';
+  }
+
+  public render() {
     return (
       <div className="score-board">
-        <span className="title">Die Besten der Besten:</span>
-        { this.getRanking() }
+        <div className="header">
+          <div className="background" />
+          <div className="icon" style={{ backgroundImage: `url(${this.getHeaderIconPath()})` }} />
+          <div className="title">{this.getTitle()}</div>
+        </div>
+        <div className="ranking-list">
+          <div className="fading-top" />
+          {this.getRanking()}
+        </div>
       </div>
     );
   }
