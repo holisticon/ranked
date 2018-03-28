f623655214125d9964930fe35e2a4aaaee698866
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-03-28 14:41:24 +0200

    Refactored players elo ranking chart to implement new charts and key figures easier

--- frontend/src/main/js/statistics/pages/score-board.tsx
+++ frontend/src/main/js/statistics/pages/score-board.tsx
@@ -1,25 +1,12 @@
 import * as React from 'react';
-import axios from 'axios';
 import './score-board.css';
 import { Player } from '../../types/types';
-import { PlayerIcon } from '../../components/player-icon';
-
-type EloData = {
-  userName: {
-    value: string;
-  },
-  elo: number;
-};
-
-interface PlayerWithElo extends Player {
-  score: number;
-}
+import { RankingChart } from '../components/ranking-chart';
+import { ChartData2D } from '../types';
+import { EloAdapter } from '../adapter/elo-adapter';
 
 type ScoreBoardState = {
-  playersMap: { [id: string]: Player },
-  players: Array<PlayerWithElo>,
-  maxElo: number,
-  minElo: number
+  playerValues: ChartData2D<Player, number>,
 };
 
 export class ScoreBoard extends React.Component<any, ScoreBoardState> {
@@ -27,80 +14,14 @@ export class ScoreBoard extends React.Component<any, ScoreBoardState> {
     super(props);
 
     // init data
-    this.getAllPlayers().then(playersMap => {
-      this.setState({ ...this.state, playersMap });
-      this.updateList();
-    });
+    this.updateList();
     setInterval(() => this.updateList(), 60 * 1000);
   }
 
   private updateList(): void {
-    if (!this.state.playersMap) {
-      return;
-    }
-
-    this.getPlayersScore().then(eloData => {
-      const players = eloData.map(elo => {
-        return {
-          ...this.state.playersMap[elo.userName.value],
-          score: elo.elo
-        };
-      });
-
-      this.setState({
-        players,
-        maxElo: eloData[0].elo,
-        minElo: eloData[eloData.length - 1].elo
-      });
+    EloAdapter.getEloData().then(playerElos => {
+      this.setState({ playerValues: playerElos });
     });
-  }
-
-  private getAllPlayers(): Promise<{ [id: string]: Player }> {
-    return axios.get('/view/user')
-      .then(res => res.data as Array<Player>)
-      .then(players => players.reduce(
-        (map, player) => {
-          map[player.id] = player;
-          return map;
-        },
-        {})
-      );
-  }
-
-  private getPlayersScore(): Promise<Array<EloData>> {
-    return axios.get('/view/elo/player')
-      .then(res => res.data);
-  }
-
-  private calcEloPercentage(elo: number): number {
-    const min = this.state.minElo;
-    const max = this.state.maxElo;
-
-    return (elo - min) / (max - min);
-  }
-
-  private getRanking() {
-    if (!this.state || !this.state.players) {
-      return;
-    }
-
-    return this.state.players
-      .map((player, i) => {
-        const barWidth = this.calcEloPercentage(player.score) * 70 + 30;
-
-        return (
-          <div key="i" className="ranking-entry">
-            <div className="icon">
-              <PlayerIcon img={player.imageUrl} click={() => { return; }} />
-            </div>
-            <div className="name">{player.name}</div>
-            <div className="bar">
-              <div className="bar-inner" style={{ width: barWidth + '%' }} />
-            </div>
-            <div className="score">{player.score}</div>
-          </div>
-        );
-      });
   }
 
   private getHeaderIconPath(): string {
@@ -121,7 +42,7 @@ export class ScoreBoard extends React.Component<any, ScoreBoardState> {
         </div>
         <div className="ranking-list">
           <div className="fading-top" />
-          {this.getRanking()}
+          <RankingChart data={ !this.state ? undefined : this.state.playerValues } />
         </div>
       </div>
     );
