45f93e5097036cebb6ac9470336f7034494de73f
Timo Gröger <timo.groeger@holistion.de>
2018-03-28 16:35:21 +0200

    Implemented Carousel and second statistic

--- frontend/src/main/js/statistics/adapter/players-adapter.ts
+++ frontend/src/main/js/statistics/adapter/players-adapter.ts
@@ -4,12 +4,12 @@ import { PlayerService } from '../../services/player-service';
 export namespace PlayersAdapter {
 
   let playersCache: { [id: string]: Player };
-  
+
   export function getPlayersMap(): Promise<{ [id: string]: Player }> {
     if (!playersCache) {
-      playersCache = {};
       return PlayerService.getAllPlayers()
         .then(players => {
+          playersCache = {};
           players.forEach(player => {
             playersCache[player.id] = player;
           });
--- frontend/src/main/js/statistics/pages/score-board.tsx
+++ frontend/src/main/js/statistics/pages/score-board.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
-import './score-board.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Player } from '../../types/types';
 import { RankingChart } from '../components/ranking-chart';
 import { ChartData2D } from '../types';
 import { EloAdapter } from '../adapter/elo-adapter';
+import { GoalsAdapter } from '../adapter/goals-adapter';
+import './score-board.css';
 
 type ScoreBoardState = {
   playerValues: ChartData2D<Player, number>,
+  playerGoals: ChartData2D<Player, number>,
+  headings: Array<{ title: string, icon: string }>,
+  currentCarouselIndex: number
 };
 
 export class ScoreBoard extends React.Component<any, ScoreBoardState> {
   constructor(props: any) {
     super(props);
+    this.state = { ...this.state, currentCarouselIndex: 0, headings: [] };
 
     // init data
     this.updateList();
@@ -19,17 +26,30 @@ export class ScoreBoard extends React.Component<any, ScoreBoardState> {
   }
 
   private updateList(): void {
-    EloAdapter.getEloData().then(playerElos => {
-      this.setState({ playerValues: playerElos });
-    });
+    Promise.all([EloAdapter.getEloData(), GoalsAdapter.getTotalGoalsData()])
+      .then(([eloData, totalGoalsData]) => {
+        const headings = [
+          { title: 'Holisticon AllStars', icon: '/img/trophy.png' },
+          { title: 'Erzielte Tore', icon: '/img/goal.png' }
+        ];
+        this.setState({ playerValues: eloData, playerGoals: totalGoalsData, headings });
+      });
   }
 
   private getHeaderIconPath(): string {
-    return '/img/trophy.png';
+    if (this.state.currentCarouselIndex >= this.state.headings.length) {
+      return '';
+    }
+
+    return this.state.headings[this.state.currentCarouselIndex].icon;
   }
 
   private getTitle(): string {
-    return 'Holisticon AllStars';
+    if (this.state.currentCarouselIndex >= this.state.headings.length) {
+      return '';
+    }
+
+    return this.state.headings[this.state.currentCarouselIndex].title;
   }
 
   public render() {
@@ -40,10 +60,16 @@ export class ScoreBoard extends React.Component<any, ScoreBoardState> {
           <div className="icon" style={{ backgroundImage: `url(${this.getHeaderIconPath()})` }} />
           <div className="title">{this.getTitle()}</div>
         </div>
-        <div className="ranking-list">
-          <div className="fading-top" />
-          <RankingChart data={ !this.state ? undefined : this.state.playerValues } />
-        </div>
+        <Carousel swipeScrollTolerance={130} onChange={ (index) => this.setState({ currentCarouselIndex: index }) } autoPlay={true} showThumbs={false} infiniteLoop={true} interval={10000} showStatus={false} showArrows={false}>
+          <div className="ranking-list">
+            <div className="fading-top" />
+            <RankingChart data={ !this.state ? undefined : this.state.playerValues } />
+          </div>
+          <div className="ranking-list">
+            <div className="fading-top" />
+            <RankingChart data={ !this.state ? undefined : this.state.playerGoals } />
+          </div>
+        </Carousel>
       </div>
     );
   }
