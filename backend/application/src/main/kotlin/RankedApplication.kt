4bd3f73e6bdd4b5905a67c65003f839aff0f24e0
Jan Galinski <jan.galinski@holisticon.de>
2018-03-30 14:25:52 +0200

    update to springboot release, remove swagger-starter, fixes #60

--- backend/application/src/main/kotlin/RankedApplication.kt
+++ backend/application/src/main/kotlin/RankedApplication.kt
@@ -2,6 +2,15 @@ package de.holisticon.ranked
 
 import de.holisticon.ranked.extension.runApplicationExpr
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
  * Main application -> run this!
@@ -10,3 +19,42 @@ fun main(args: Array<String>) = runApplicationExpr<RankedApplication>(*args)
 
 @SpringBootApplication
 class RankedApplication
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+
+  @Bean
+  fun viewDocket() = docket(
+    groupName = "Views",
+    title = "Ranked View API",
+    description = "View API to access different views in ranked.",
+    basePackage = "de.holisticon.ranked.view",
+    path = "/view/**")
+
+  @Bean
+  fun commandDocket() = docket(
+    groupName = "Commands",
+    title = "Ranked Command API",
+    description = "Command API to write to ranked.",
+    basePackage = "de.holisticon.ranked.command",
+    path = "/command/**")
+
+}
+
+
+private fun docket(groupName: String, title: String, description: String, basePackage: String, path: String) = Docket(DocumentationType.SWAGGER_2)
+  .groupName(groupName)
+  .apiInfo(ApiInfoBuilder()
+    .title(title)
+    .description(description)
+    .termsOfServiceUrl("None")
+    .version("1.0.0")
+    .contact(Contact("Holisticon Craftsmen", "https://www.holisticon.de", "jobs@holisticon.de"))
+    .license("Revised BSD License")
+    .licenseUrl("https://github.com/holisticon/ranked/blob/master/LICENSE.txt")
+    .build())
+  .select()
+  .apis(RequestHandlerSelectors.basePackage(basePackage))
+  .paths(PathSelectors.ant(path))
+  .build()
