f8cfc4861f8c88c5e69cda47db400a73641e9014
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-04-27 11:41:46 +0200

    Calculate winner team correctly in WallService

--- frontend/src/main/js/services/wall.service.ts
+++ frontend/src/main/js/services/wall.service.ts
@@ -24,16 +24,25 @@ export namespace WallService {
     });
 
   export function playedMatches(): Observable<Array<PlayedMatch>> {
-    return matchWall.map(matches => matches.map(match => ({
-      team1: {
-        player1: match.teamBlue.player1.value,
-        player2: match.teamBlue.player2.value
-      },
-      team2: {
-        player1: match.teamRed.player1.value,
-        player2: match.teamRed.player2.value
-      },
-      winner: 'team1'
-    }) as PlayedMatch));
+    return matchWall.map(matches => 
+      matches.map(match => {
+        let wonSetsBlue = 0;
+        let wonSetsRed = 0;
+
+        match.matchSets.forEach(set => set.goalsBlue > set.goalsRed ? wonSetsBlue++ : wonSetsRed++);
+
+        return {
+          team1: {
+            player1: match.teamBlue.player1.value,
+            player2: match.teamBlue.player2.value
+          },
+          team2: {
+            player1: match.teamRed.player1.value,
+            player2: match.teamRed.player2.value
+          },
+          winner: wonSetsBlue > wonSetsRed ? 'team1' : 'team2'
+        } as PlayedMatch;
+      })
+    );
   }
 }
