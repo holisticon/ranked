3d999537a575d248574b6dd595d59bb4cd3e3611
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-06-01 17:51:10 +0200

    Added TeamIcon

---
+++ frontend/src/main/js/components/team-icon.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Team } from '../types/types';
+import { PlayerIcon } from './player-icon';
+import './team-icon.css';
+
+export interface TeamIconProps {
+  team: Team;
+}
+
+export class TeamIcon extends React.Component<TeamIconProps, any> {
+  constructor(props: TeamIconProps) {
+    super(props);
+  }
+
+  render() {
+    if (!this.props.team.imageUrl) {
+      return (
+        <div className="team-icon">
+          <div className="half">
+            <PlayerIcon
+              click={() => { return; } }
+              img={ this.props.team.player1.imageUrl }
+              name={ this.props.team.player1.displayName }
+            />
+          </div>
+          <div className="half">
+            <PlayerIcon
+              click={() => { return; } }
+              img={ this.props.team.player2.imageUrl }
+              name={ this.props.team.player2.displayName }
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <PlayerIcon click={() => { return; } } img={ this.props.team.imageUrl } />
+      );
+    }
+  }
+}
