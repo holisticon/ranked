2c22e804c4e81c3da82048498bc1711a9bf0a26f
Timo Gröger <timo.groeger@holistion.de>
2018-07-13 10:54:56 +0200

    add elo history by player

--- backend/views/leaderboard/src/main/kotlin/PlayerRankingByElo.kt
+++ backend/views/leaderboard/src/main/kotlin/PlayerRankingByElo.kt
@@ -8,10 +8,14 @@ import de.holisticon.ranked.model.event.PlayerRankingChanged
 import mu.KLogging
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
+import org.axonframework.eventhandling.Timestamp
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicReference
 import java.util.function.Supplier
 
@@ -21,12 +25,18 @@ const val PROCESSING_GROUP = "PlayerLeaderBoard"
  * The rest controller just provides a facade for [PlayerRankingByEloHandler.get].
  */
 @RestController
-@RequestMapping(value = ["/view"])
+@RequestMapping(value = ["/view/elo"])
 class PlayerLeaderBoardView(
   private val playerRankingByElo: PlayerRankingByEloHandler
 ) {
-  @GetMapping(path = ["/elo/player"])
+  @GetMapping(path = ["/player"])
   fun userListByEloRank() : List<PlayerElo> = playerRankingByElo.get()
+
+  @GetMapping(path = ["/player/{userName}"])
+  fun eloHistoryByPlayer(@PathVariable("userName") userName: String): MutableList<Pair<LocalDateTime, Elo>>? = playerRankingByElo.getHistory(userName)
+
+
+
 }
 
 /**
@@ -44,6 +54,11 @@ class PlayerRankingByEloHandler : Supplier<List<PlayerElo>> {
   private val ranking = mutableMapOf<UserName, Elo>()
 
   /**
+   * Holds a mutable list of userName->List<timestamp, elo>, this is the history of elo values that is updated with every elo change.
+   */
+  private val rankingHistory = mutableMapOf<UserName, MutableList<Pair<LocalDateTime, Elo>>>()
+
+  /**
    * The cache holds an immutable list of [PlayerElo] for the json return. It is sorted descending by elo value.
    * A cache is used because we only need to sort once per update and can return the same immutable sorted list
    * for every get request after that.
@@ -51,13 +66,19 @@ class PlayerRankingByEloHandler : Supplier<List<PlayerElo>> {
   private val cache = AtomicReference<List<PlayerElo>>(listOf())
 
   @EventHandler
-  fun on(e: PlayerRankingChanged) {
+  fun on(e: PlayerRankingChanged, @Timestamp t: Instant) {
     logger.debug { "Player '${e.player}' new rating is '${e.eloRanking}'" }
     ranking[e.player] = e.eloRanking
+
+    rankingHistory.putIfAbsent(e.player, mutableListOf())
+    rankingHistory[e.player]!!.add(Pair(LocalDateTime.from(t), e.eloRanking))
+
     cache.set(ranking.map { it.toPair() }.map { PlayerElo(it.first, it.second) }.sorted())
   }
 
   override fun get() = cache.get()!!
+
+  fun getHistory(userName: String) = rankingHistory[UserName(userName)]
 }
 
 /**
