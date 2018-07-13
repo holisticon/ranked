70b6216cf6be908adeb59e4db215f5b4941ac51f
Oliver Niebsch <oliver.niebsch@holisticon.de>
2018-07-13 10:35:42 +0200

    Added average setDuration and matchDuration in leaderboard views

--- backend/views/leaderboard/src/main/kotlin/PlayerRankingByMatches.kt
+++ backend/views/leaderboard/src/main/kotlin/PlayerRankingByMatches.kt
@@ -3,14 +3,18 @@
 package de.holisticon.ranked.view.leaderboard
 
 import de.holisticon.ranked.model.Team
+import de.holisticon.ranked.model.TimedMatchSet
 import de.holisticon.ranked.model.UserName
+import de.holisticon.ranked.model.event.MatchCreated
 import de.holisticon.ranked.model.event.TeamWonMatch
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.temporal.ChronoUnit
 
 /**
  * The rest controller provides getters for key figures based on the played sets
@@ -20,39 +24,69 @@ import org.springframework.web.bind.annotation.RestController
 class PlayerRankingByMatchesView(
     private val playerRankingByMatches: PlayerRankingByMatches
 ) {
-  @GetMapping(path = ["/count"])
-  fun userListByGoalSum(): Set<PlayerMatchCount> = playerRankingByMatches.getMatchCount()
+  @GetMapping(path = ["/player/{userName}"])
+  fun matchStatsByPlayer(@PathVariable("userName") userName: String): PlayerMatchStats? = playerRankingByMatches.getMatchStatsForPlayer(userName)
 }
 
 @Component
 @ProcessingGroup(PROCESSING_GROUP)
 class PlayerRankingByMatches {
 
-  private val matchCount = mutableMapOf<UserName, PlayerMatchCount>()
+  private val matchStats = mutableMapOf<UserName, PlayerMatchStats>()
+  private val matches: MutableMap<String, MatchCreated> = mutableMapOf()
+
+  @EventHandler
+  fun on(e: MatchCreated) {
+    matches[e.matchId] = e
+  }
 
   @EventHandler
   fun on(e: TeamWonMatch) {
+    val match = matches[e.matchId] ?: throw IllegalStateException("Match won without being created.")
+    ensurePlayerStatExists(e.team)
+    ensurePlayerStatExists(e.looser)
+
+    val lastSet = match.matchSets.last();
+    if (lastSet is TimedMatchSet) {
+      val matchTime = ChronoUnit.SECONDS.between(match.startTime.toLocalTime(), lastSet.goals.last().second.toLocalTime())
+      setAvgMatchTimeForPlayersOfTeam(e.team, matchTime)
+      setAvgMatchTimeForPlayersOfTeam(e.looser, matchTime)
+    }
+
     incWonMatchesForPlayersOfTeam(e.team)
     incLostMatchesForPlayersOfTeam(e.looser)
   }
 
-  private fun incWonMatchesForPlayersOfTeam(team: Team) {
-    matchCount.putIfAbsent(team.player1, PlayerMatchCount(team.player1, 0, 0))
-    matchCount.putIfAbsent(team.player2, PlayerMatchCount(team.player2, 0, 0))
+  private fun ensurePlayerStatExists(team: Team) {
+    matchStats.putIfAbsent(team.player1, PlayerMatchStats(team.player1, 0, 0, 0.0))
+    matchStats.putIfAbsent(team.player2, PlayerMatchStats(team.player2, 0, 0, 0.0))
+  }
 
-    matchCount[team.player1]!!.wonMatches++
-    matchCount[team.player2]!!.wonMatches++
+  private fun setAvgMatchTimeForPlayersOfTeam(team: Team, matchTime: Long) {
+    setAvgMatchTimeForPlayer(team.player1, matchTime);
+    setAvgMatchTimeForPlayer(team.player2, matchTime);
+  }
+
+  private fun setAvgMatchTimeForPlayer(player: UserName, matchTime: Long) {
+    val stats = matchStats[player]!!;
+    stats.averageMatchTime = calcAvgMatchTime(stats.lostMatches + stats.wonMatches, stats.averageMatchTime, matchTime);
+  }
+
+  private fun calcAvgMatchTime(matchCount: Int, averageMatchTime: Double, matchTime: Long): Double {
+    return (averageMatchTime * matchCount + matchTime) / (matchCount + 1);
+  }
+
+  private fun incWonMatchesForPlayersOfTeam(team: Team) {
+    matchStats[team.player1]!!.wonMatches++
+    matchStats[team.player2]!!.wonMatches++
   }
 
   private fun incLostMatchesForPlayersOfTeam(team: Team) {
-    matchCount.putIfAbsent(team.player1, PlayerMatchCount(team.player1, 0, 0))
-    matchCount.putIfAbsent(team.player2, PlayerMatchCount(team.player2, 0, 0))
-
-    matchCount[team.player1]!!.lostMatches++
-    matchCount[team.player2]!!.lostMatches++
+    matchStats[team.player1]!!.lostMatches++
+    matchStats[team.player2]!!.lostMatches++
   }
 
-  fun getMatchCount() = matchCount.values.toSet()
+  fun getMatchStatsForPlayer(playerName: String) = matchStats[UserName(playerName)]
 }
 
-data class PlayerMatchCount(val userName: UserName, var wonMatches: Int, var lostMatches: Int)
+data class PlayerMatchStats(val userName: UserName, var wonMatches: Int, var lostMatches: Int, var averageMatchTime: Double)
--- backend/views/leaderboard/src/main/kotlin/PlayerRankingBySets.kt
+++ backend/views/leaderboard/src/main/kotlin/PlayerRankingBySets.kt
@@ -4,14 +4,17 @@ package de.holisticon.ranked.view.leaderboard
 
 import de.holisticon.ranked.model.Team
 import de.holisticon.ranked.model.TeamColor
+import de.holisticon.ranked.model.TimedMatchSet
 import de.holisticon.ranked.model.UserName
 import de.holisticon.ranked.model.event.MatchCreated
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.temporal.ChronoUnit
 
 /**
  * The rest controller provides getters for key figures based on the played sets
@@ -21,22 +24,32 @@ import org.springframework.web.bind.annotation.RestController
 class PlayerRankingBySetsView(
     private val playerRankingBySets: PlayerRankingBySets
 ) {
-  @GetMapping(path = ["/count"])
-  fun userListByGoalSum(): Set<PlayerSetCount> = playerRankingBySets.getSetCount()
-
-  /*@GetMapping(path = ["/player/{userName}"])
-  fun goalStatsByPlayer(@PathVariable("userName") userName: String): PlayerGoalStats = playerRankingByGoals.getGoalStatsForPlayer(userName)*/
+  @GetMapping(path = ["/player/{userName}"])
+  fun setStatsByPlayer(@PathVariable("userName") userName: String): PlayerSetStats? = playerRankingBySets.getSetStatsForPlayer(userName)
 }
 
 @Component
 @ProcessingGroup(PROCESSING_GROUP)
 class PlayerRankingBySets {
 
-  private val setCount = mutableMapOf<UserName, PlayerSetCount>()
+  private val setStats = mutableMapOf<UserName, PlayerSetStats>()
 
   @EventHandler
   fun on(e: MatchCreated) {
+    ensurePlayerStatExists(e.teamBlue);
+    ensurePlayerStatExists(e.teamRed);
+
+    var lastSetEndTime = e.startTime.toLocalTime();
+
     e.matchSets.forEach {
+      if (it is TimedMatchSet) {
+        val endTime = it.goals.last().second.toLocalTime()
+        val setTime = ChronoUnit.SECONDS.between(lastSetEndTime, endTime)
+        setAvgSetTimeForPlayersOfTeam(e.teamRed, setTime)
+        setAvgSetTimeForPlayersOfTeam(e.teamBlue, setTime)
+        lastSetEndTime = endTime
+      }
+
       if (it.winner() === TeamColor.RED) {
         incWonSetsForPlayersOfTeam(e.teamRed, it.offenseRed)
         incLostSetsForPlayersOfTeam(e.teamBlue, it.offenseBlue)
@@ -47,42 +60,57 @@ class PlayerRankingBySets {
     }
   }
 
-  private fun incWonSetsForPlayersOfTeam(team: Team, offense: UserName) {
-    setCount.putIfAbsent(team.player1, PlayerSetCount(team.player1, 0, 0, 0, 0))
-    setCount.putIfAbsent(team.player2, PlayerSetCount(team.player2, 0, 0, 0, 0))
+  private fun ensurePlayerStatExists(team: Team) {
+    setStats.putIfAbsent(team.player1, PlayerSetStats(team.player1, 0, 0, 0, 0, 0.0))
+    setStats.putIfAbsent(team.player2, PlayerSetStats(team.player2, 0, 0, 0, 0, 0.0))
+  }
 
+  private fun incWonSetsForPlayersOfTeam(team: Team, offense: UserName) {
     if (team.player1 == offense) {
-      setCount[team.player1]!!.incWonSetsInOffense()
-      setCount[team.player2]!!.incWonSetsInDefense()
+      setStats[team.player1]!!.incWonSetsInOffense()
+      setStats[team.player2]!!.incWonSetsInDefense()
     } else if (team.player2 == offense) {
-      setCount[team.player1]!!.incWonSetsInDefense()
-      setCount[team.player2]!!.incWonSetsInOffense()
+      setStats[team.player1]!!.incWonSetsInDefense()
+      setStats[team.player2]!!.incWonSetsInOffense()
     }
   }
 
   private fun incLostSetsForPlayersOfTeam(team: Team, offense: UserName) {
-    setCount.putIfAbsent(team.player1, PlayerSetCount(team.player1, 0, 0, 0, 0))
-    setCount.putIfAbsent(team.player2, PlayerSetCount(team.player2, 0, 0, 0, 0))
-
     if (team.player1 == offense) {
-      setCount[team.player1]!!.incLostSetsInOffense()
-      setCount[team.player2]!!.incLostSetsInDefense()
+      setStats[team.player1]!!.incLostSetsInOffense()
+      setStats[team.player2]!!.incLostSetsInDefense()
     } else if (team.player2 == offense) {
-      setCount[team.player1]!!.incLostSetsInDefense()
-      setCount[team.player2]!!.incLostSetsInOffense()
+      setStats[team.player1]!!.incLostSetsInDefense()
+      setStats[team.player2]!!.incLostSetsInOffense()
     }
   }
 
-  fun getSetCount() = setCount.values.toSet()
+  private fun setAvgSetTimeForPlayersOfTeam(team: Team, setTime: Long) {
+    setAvgSetTimeForPlayer(team.player1, setTime);
+    setAvgSetTimeForPlayer(team.player2, setTime);
+  }
+
+  private fun setAvgSetTimeForPlayer(player: UserName, setTime: Long) {
+    val stats = setStats[player]!!;
+    val totalSets = stats.lostSets.whenInOffense + stats.lostSets.whenInDefense + stats.wonSets.whenInOffense + stats.wonSets.whenInDefense;
+    stats.averageMatchTime = calcAvgSetTime(totalSets, stats.averageMatchTime, setTime);
+  }
+
+  private fun calcAvgSetTime(setCount: Int, averageSetTime: Double, setTime: Long): Double {
+    return (averageSetTime * setCount + setTime) / (setCount + 1);
+  }
+
+  fun getSetStatsForPlayer(playerName: String) = setStats[UserName(playerName)]
 }
 
-data class PlayerSetCount(val userName: UserName, val wonSets: InPosition<Int>, val lostSets: InPosition<Int>) {
+data class PlayerSetStats(val userName: UserName, val wonSets: InPosition<Int>, val lostSets: InPosition<Int>, var averageMatchTime: Double) {
 
   constructor(userName: UserName, wonSetsInOffense: Int, wonSetsInDefense: Int,
-              lostSetsInOffense: Int, lostSetsInDefense: Int) :
+              lostSetsInOffense: Int, lostSetsInDefense: Int, averageMatchTime: Double) :
       this(userName,
           InPosition(wonSetsInOffense, wonSetsInDefense),
-          InPosition(lostSetsInOffense, lostSetsInDefense))
+          InPosition(lostSetsInOffense, lostSetsInDefense),
+          averageMatchTime)
 
   fun incWonSetsInOffense() {
     wonSets.whenInOffense++
